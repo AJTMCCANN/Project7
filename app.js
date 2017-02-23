2a982be60b219f220f1cbdff5c9072edb9e462de
AJTMCCANN <adam.jt.mccann@gmail.com>
2017-02-23 02:16:54 -0700

    all three columns now display api data

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var moment = require('moment')
 var Twit = require('twit')
 var CONFIG = require('./config.js')
 
@@ -8,15 +9,14 @@ var app = express()
 
 var friend_count = new Promise( (resolve, reject) => {
   T.get('users/show', {user_id: 15727386}, function(error, user, response) {
-    if (error) {console.error(error)}
-    console.log(user.friends_count)
+    if (error) { reject(error) }
     resolve(user.friends_count)
   });
 })
 
 var friends = new Promise ( (resolve, reject) => {
   T.get('friends/list', {count: 5}, function(error, friends, response) {
-    if (error) {console.error(error)}
+    if (error) { reject(error) }
     var frnds = friends.users.map( function(friend, index) {
       var new_friend = {}
       new_friend._id = index
@@ -25,14 +25,13 @@ var friends = new Promise ( (resolve, reject) => {
       new_friend.avatar_img = friend.profile_image_url
       return new_friend
     })
-    // console.log(frnds)
     resolve(frnds)
   })
 })
 
 var tweets = new Promise( (resolve, reject) => {
   T.get('statuses/user_timeline', {count: 5}, function(error, tweets, response) {
-    if (error) {console.error(error)}
+    if (error) { reject(error) }
     var twts = tweets.map( function(tweet, index) {
       var new_tweet = {}
       new_tweet._id = index
@@ -45,50 +44,62 @@ var tweets = new Promise( (resolve, reject) => {
       new_tweet.like_count = tweet.favorite_count
       return new_tweet
     })
-    // console.log(twts)
     resolve(twts)
   })
 })
-//
-// function get_direct_messages_to_me() {
-//   T.get('direct_messages', {count: 5}, function(error, messages, response) {
-//     if (error) {console.error(error)}
-//     var msgs = messages.map( function(message, index) {
-//       var new_msg = {}
-//       new_msg._id = index
-//       new_msg.avatar_img = message.sender.profile_image_url
-//       new_msg.text = message.text
-//       new_msg.timestamp = message.created_at
-//       new_msg.origin = "app--message"
-//       return new_msg
-//     })
-//     // console.log(msgs)
-//     return msgs
-//   })
-// }
-//
-// function get_direct_messages_from_me() {
-//     T.get('direct_messages/sent', {count: 5}, function(error, messages, response) {
-//       if (error) {console.error(error)}
-//       var msgs = messages.map( function(message, index) {
-//         var new_msg = {}
-//         new_msg._id = index
-//         new_msg.avatar_img = message.sender.profile_image_url
-//         new_msg.text = message.text
-//         new_msg.timestamp = message.created_at
-//         new_msg.origin = "app--message--me"
-//         return new_msg
-//       })
-//       // console.log(msgs)
-//       return msgs
-//     })
-// }
+
+var direct_to_me = new Promise( (resolve, reject) => {
+  T.get('direct_messages', {count: 3}, function(error, messages, response) {
+    if (error) { reject(error) }
+    var msgs = messages.map( function(message, index) {
+      var new_msg = {}
+      new_msg._id = index
+      new_msg.avatar_img = message.sender.profile_image_url
+      new_msg.text = message.text
+      new_msg.timestamp = message.created_at
+      new_msg.origin = "app--message"
+      return new_msg
+    })
+    resolve(msgs)
+  })
+})
+
+var direct_from_me = new Promise( (resolve, reject) => {
+    T.get('direct_messages/sent', {count: 2}, function(error, messages, response) {
+      if (error) { reject(error) }
+      var msgs = messages.map( function(message, index) {
+        var new_msg = {}
+        new_msg._id = index
+        new_msg.avatar_img = message.sender.profile_image_url
+        new_msg.text = message.text
+        new_msg.timestamp = message.created_at
+        new_msg.origin = "app--message--me"
+        return new_msg
+      })
+      resolve(msgs)
+    })
+})
+
+
+var direct_msgs = new Promise( (resolve, reject) => {
+  Promise.all([direct_to_me, direct_from_me]).then( (values) => {
+  var all_msgs = values[0].concat(values[1])
+  all_msgs.map( (msg, index) => {
+    how_long_ago = moment(msg.timestamp, "ddd MMM D HH:mm:ss ZZ YYYY").from(moment())
+    msg["how_long_ago"] = how_long_ago
+    new_timestamp = parseInt(moment(msg.timestamp, "ddd MMM D HH:mm:ss ZZ YYYY").format("x"))
+    msg.timestamp = new_timestamp
+    msg._id = index
+    return msg
+  })
+  sorted_msgs = all_msgs.sort((a,b) => {return (a.timestamp - b.timestamp)})
+  console.log(sorted_msgs)
+  resolve(sorted_msgs)
+})})
 
 //TODO: Combine the two get_direct_messages functions into a get_sorted_messages function
 
-promises = [friend_count, friends, tweets]
-
-console.log(typeof friend_count)
+promises = [friend_count, friends, tweets, direct_msgs]
 
 app.set('view engine', 'pug')
 app.set('views', __dirname +'/views')
@@ -96,7 +107,7 @@ app.use(express.static('public'))
 
 app.get('/pug', function(req, res) {
   Promise.all(promises).then( (values) => {
-    res.render('template.pug', {friend_count: values[0], friends: values[1], tweets: values[2]})
+    res.render('template.pug', {friend_count: values[0], friends: values[1], tweets: values[2], messages: values[3]})
   })
 })
 
